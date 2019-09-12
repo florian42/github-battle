61bb96b16d0d7985fe8e79f44ff8851afdd6871e
Florian Aumeier (FG-732) <Florian.AB.Aumeier@bmw.de>
2019-09-12 15:55:33 +0200

    first component

---
+++ app/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom' // Is seperate from react since you might want to render it in a different environment, like android or ios
+import './index.css'
+
+// Component (Aspects of a component)
+//  State
+//  Lifecycle
+//  UI
+
+// ES& class
+class App extends React.Component {
+    render() {
+        // Violation of Seperation of Concerns?
+        // A. refer to Aspects of a component -> A component is concerned about the ui layer
+        // JSX will be converted into js by Babel: React.createElement
+        return (
+        <div> 
+            Hello World
+        </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    // 1. Param: React Element,
+    <App/>,
+    // 2. Param: Where to render the element to
+    document.getElementById('app')
+
+)
