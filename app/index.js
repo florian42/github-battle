ef5e6e9488ba07c3565354bde8afd375a0470876
Florian Aumeier <florian.aumeier@protonmail.com>
2019-09-15 18:10:02 +0200

    added Popular languages buttons

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom' // Is seperate from react since you might want to render it in a different environment, like android or ios
 import './index.css'
+import Popular from './components/Popular'
 
 // Component (Aspects of a component)
 //  State
@@ -14,8 +15,8 @@ class App extends React.Component {
         // A. refer to Aspects of a component -> A component is concerned about the ui layer
         // JSX will be converted into js by Babel: React.createElement
         return (
-        <div> 
-            Hello World
+        <div className='container'>
+            <Popular />
         </div>
         )
     }
