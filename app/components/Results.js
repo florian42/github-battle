bf10e2c11a90bb85f96ff61ce5c8c22c32eed011
Florian Aumeier <florian.aumeier@protonmail.com>
2019-09-17 20:45:36 +0200

    feat: loading results data into Results

--- app/components/Results.js
+++ app/components/Results.js
@@ -2,18 +2,39 @@ import React from 'react'
 import { battle } from '../utils/api'
 
 export default class Results extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      winner: null,
+      looser: null,
+      error: null,
+      message: null,
+      loading: true
+    }
+  }
   componentWillMount() {
-    const { playerOne, playerTwo } = this.props
+    const { playerOne, playerTwo, } = this.props
     battle([playerOne, playerTwo])
       .then((players) => {
-        console.log('data: ', players)
+        this.setState({
+          winner: players[0],
+          looser: players[1],
+          error: null,
+          loading: false
+        })
+      }).catch(({ message }) => {
+        this.setState({
+          message: message,
+          loading: false
+        })
       })
   }
   render() {
     return (
       <div>
         Results
-        <pre>{JSON.stringify(this.props, null, 2)}</pre>
+        <pre>{JSON.stringify(this.state, null, 2)}</pre>
       </div>
     )
   }
