d6fd876d728ad5dd8f60e2211e5afb6e7210e187
Florian <f.aumeier@me.com>
2019-10-01 15:14:43 +0200

    feat: theme provider

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom' // Is seperate from react since you might want 
 import './index.css'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
+import { ThemeProvider } from './contexts/theme'
 
 // Component (Aspects of a component)
 //  State
@@ -11,14 +12,28 @@ import Battle from './components/Battle'
 
 // ES& class
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
+    }
+  }
   render() {
     // Violation of Seperation of Concerns?
     // A. refer to Aspects of a component -> A component is concerned about the ui layer
     // JSX will be converted into js by Babel: React.createElement
     return (
-      <div className='container'>
-        <Battle />
-      </div>
+      <ThemeProvider>
+        <div className='container'>
+          <Battle />
+        </div>
+      </ThemeProvider>
     )
   }
 }
